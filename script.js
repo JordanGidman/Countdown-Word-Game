b9d3c9faaa56c2253edfc9b4297233aed4f40a9f
JordanGidman <jordanleegidman@hotmail.com>
2022-08-28 18:13:11 +0100

    Finishing out functionality and features
    Today i spent the day implementing the missing features to get the app to run such as the buttons functionality and the randomaization of the generated letter sequence. I also added the media queries to make the app responsive. I added a light and dark mode toggle button. I also added some form validation. i am not preventing the user from entering anything unless the round has not started however i am ensuring that they cannot be considered as correct answers. Next i implemented a persistent highscore and as it is only a single number that i need to keep stored i am utilizing local storage for this. I have also added a rules modal to explain the games rules and scoring system.

--- script.js
+++ script.js
@@ -10,34 +10,57 @@ const inputForm = document.querySelector(`.input-form`);
 const scoreEl = document.querySelector(`.curr-score`);
 const scoreHighEl = document.querySelector(`.high-score`);
 const inputLabel = document.querySelector(`.input-label`);
+const roundNumEl = document.querySelector(`.round-num`);
+const btnToggleColor = document.querySelector(`.input-toggle`);
+const pageContainer = document.querySelector(`.container`);
+const burgerMenu = document.querySelector(`.burger-menu`);
+const headerEl = document.querySelector(`.header`);
+const btnCloseModal = document.querySelector(`.btn-close-modal`);
+
+console.log(burgerMenu);
+console.log(localStorage);
+
+const saveScore = function () {
+  localStorage.setItem("savedScore", highScore);
+};
+
+const getHighscore = function () {
+  return localStorage.getItem("savedScore");
+};
+
+console.log(getHighscore());
 
 const characters = `bcdfghjklmnpqrstvwxyz`;
 const vowels = `aeiou`;
 //How many seconds the player has
-let gameTime = 10;
+let gameTime = 30;
 let timer;
 let roundNum = 1;
 let userAnswer = ``;
 let currScore = 0;
-let highScore = 0;
+let highScore = getHighscore() ? Number(getHighscore()) : 0;
 let data;
+let currWord;
 
-//we want to have a loop that will run between 5 and 7 times
+scoreHighEl.textContent = `Highscore: ${highScore}`;
+roundNumEl.textContent = `Round: ${roundNum}`;
 
+//Create the random letters
 const generateWord = function () {
   let str = [];
 
+  //we want to have a loop that will run between 5 and 7 times
   for (let i = 0; i < Math.trunc(Math.random() * (7 - 6 + 1) + 6); i++) {
     str.push(characters[Math.trunc(Math.random() * 21)]);
   }
 
   let amountLeft = 10 - str.length;
-
+  //we need to ensure a word can actually be made by ensuring that there are always some vowels
   for (let j = 0; j < amountLeft; j++) {
     str.push(vowels[Math.trunc(Math.random() * 4)]);
   }
 
-  return str;
+  return shuffle(str);
 };
 
 //TIMER
@@ -54,45 +77,19 @@ const startTimer = function () {
       randomLettersEl.textContent = `**********`;
       nextRound(`+++`);
 
-      if (roundNum < 5) {
-        roundNum++;
-      } else {
-        roundNum--; //THIS NEEDS TO BE CHANGED TO GAME OVER FUNCTION
-      }
+      // if (roundNum < 5) roundNum++;
+      // else roundNum--; //THIS NEEDS TO BE CHANGED TO GAME OVER FUNCTION
     }
     time--;
   };
   let time = gameTime;
 
-  //call timer every second
+  //call timer immediately and then every second
   ticks();
   const timer = setInterval(ticks, 1000);
 
   return timer;
 };
-
-btnStart.addEventListener(`click`, function () {
-  inputWord.removeAttribute(`readonly`);
-  console.log(inputWord.attributes);
-
-  let currWord = generateWord().join(``);
-
-  randomLettersEl.textContent = currWord;
-
-  if (timer) {
-    clearInterval(timer);
-  }
-
-  timer = startTimer();
-});
-
-btnSubmit.addEventListener(`click`, function (e) {
-  e.preventDefault();
-  userAnswer = inputWord.value;
-  // checkAnswer(userAnswer);
-  clearInterval(timer);
-  nextRound(userAnswer);
-});
 
 const options = {
   method: "GET",
@@ -111,8 +108,7 @@ const checkAnswer = function (word) {
 };
 
 const resolveAnswer = function (res) {
-  console.log(`res: ${res[0]}`);
-  if (res[0]) {
+  if (res[0] && checkLetters(userAnswer, currWord)) {
     currScore += userAnswer.length;
 
     if (currScore > highScore) {
@@ -127,6 +123,7 @@ const resolveAnswer = function (res) {
   }
   scoreEl.textContent = `Score: ${currScore}`;
   scoreHighEl.textContent = `Highscore: ${highScore}`;
+  saveScore();
 };
 
 const nextRound = function (answer) {
@@ -136,4 +133,82 @@ const nextRound = function (answer) {
   inputWord.value = ``;
   randomLettersEl.textContent = `**********`;
   inputWord.setAttribute(`readonly`, true);
+  inputLabel.textContent = `Please Enter The longest Word You Can Think of`;
+  roundNum++;
+  roundNumEl.textContent = `Round: ${roundNum}`;
 };
+
+const checkLetters = function (userInput, rLetters) {
+  return userInput.split(``).every((l) => rLetters.split(``).includes(l));
+};
+
+//Fisher yates shuffle - not designed by me.
+const shuffle = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+};
+
+btnToggleColor.addEventListener(`click`, function () {
+  btnToggleColor.classList.add(`.slider-light`);
+  pageContainer.classList.toggle(`dark`);
+});
+
+btnStart.addEventListener(`click`, function () {
+  //Allow the user to type
+  inputWord.removeAttribute(`readonly`);
+
+  currWord = generateWord().join(``);
+
+  randomLettersEl.textContent = currWord;
+
+  if (timer) {
+    clearInterval(timer);
+  }
+
+  timer = startTimer();
+});
+
+btnSubmit.addEventListener(`click`, function (e) {
+  e.preventDefault();
+  userAnswer = inputWord.value;
+  // checkAnswer(userAnswer);
+  clearInterval(timer);
+
+  if (roundNum < 5) {
+    nextRound(userAnswer);
+  } else {
+    gameOver();
+  }
+});
+
+const gameOver = function () {
+  inputLabel.textContent = `Game Over your final score this run was ${currScore}`;
+  inputLabel.style.color = "green";
+  currScore = 0;
+  scoreEl.textContent = `Score: ${currScore}`;
+  timerEl.textContent = `00:00`;
+  inputWord.value = ``;
+  randomLettersEl.textContent = `**********`;
+  inputWord.setAttribute(`readonly`, true);
+  roundNum = 0;
+  roundNumEl.textContent = `Round: ${roundNum}`;
+};
+
+console.log(headerEl);
+burgerMenu.addEventListener(`click`, function () {
+  console.log(`worked`);
+  document.querySelector(`.overlay`).classList.remove(`hidden`);
+  document.querySelector(`.modal-window`).classList.remove(`hidden`);
+  btnCloseModal.classList.remove(`hidden`);
+});
+
+btnCloseModal.addEventListener(`click`, function () {
+  document.querySelector(`.overlay`).classList.add(`hidden`);
+  document.querySelector(`.modal-window`).classList.add(`hidden`);
+  btnCloseModal.classList.add(`hidden`);
+});
